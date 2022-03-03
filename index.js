d26a135ede0eb5241deaaa830dde5e7be0aa6d83
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-02 23:55:14 -0700

    Started making a simple beat with the new values. ISSUES: seems a bit off, like the kick is starting on the wrong beat. The kick audio will drop out after a certain amount of time

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let loopBeat
 let bassSynth
+let snare
 let polySynth
 
 function setup() {
     // Initialize the bassSynth, and polySynth
     bassSynth = new Tone.MembraneSynth().toMaster()
+    snare       = new Tone.MembraneSynth().toMaster()
     polySynth = new Tone.PolySynth().toMaster()
     polySynth.set({ detune: -1200 })
     
@@ -20,16 +22,19 @@ function setup() {
 // ??? to fix a sample playback issue with the web and sound,
 //     do the following to the song function
 function song(time) {
-    bassSynth.triggerAttackRelease('C4', '8n', time)
-    polySynth.triggerAttackRelease(['C4', 'E4', 'A4'], 1)
-
+    snare.triggerAttackRelease('C4', '4n', time, 1)
     // --- data type for current bea
     //     BAR:BEATS:SIXTEENTH
     let cb = [...Tone.Transport.position]
     let bar = cb[0]
     let beat = cb[2]
     let sixt = cb[4]
-    console.log(bar)
+
+    // --- MAKE BEAT USING THE NEW VALUES ABOVE
+    // - kick
+    if (parseInt(beat) === 0) {
+        bassSynth.triggerAttackRelease('C3', '8n', time)
+    }
 }
 
 function stop() {
