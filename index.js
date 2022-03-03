4e0d82668766693a58202c0f52580fbf1552d569
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-02 21:44:38 -0700

    Am no successfully importing the module. Had to change the import statements from what was provided in the docs, as well as enable Live Server (ES6 modules need to be run on a server)

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import * as Tone from 'tone'
+import * as Tone from './node_modules/tone/build/Tone.js'
+
+console.log(Tone)
 
 // create a synth and connect it to the main ouput (your speakers)
-const synth = new Tone.Synth().toDestination()
+// const synth = new Tone.Synth().toDestination()
+// console.log(synth)
 
-// play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n")
+// const now = Tone.now()
+
+// // trigger the attack immediately
+// synth.triggerAttack("C4", now)
+
+// // wait one second before triggering the release
+// synth.triggerRelease(now + 1)
+
+// // attach a click listener to a play button
+// document.querySelector('button')?.addEventListener('click', async () => {
+//     await Tone.start()
+//     console.log('audio is ready')
+// })
