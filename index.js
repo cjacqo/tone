f8a9adc5106acc072d6dd2d2c2374b29450c81eb
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-03 00:10:05 -0700

    Changed the way iteration of beat is tracked. Removed getting an array from BBS

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 let loopBeat
-let bassSynth
-let snare
-let polySynth
+let bassSynth, snare, cymbalSynth, polySynth
+let counter
 
 function setup() {
+    counter = 0
+    
     // Initialize the bassSynth, and polySynth
     bassSynth = new Tone.MembraneSynth().toMaster()
+    cymbalSynth = new Tone.MetalSynth().toMaster()
     snare       = new Tone.MembraneSynth().toMaster()
     polySynth = new Tone.PolySynth().toMaster()
     polySynth.set({ detune: -1200 })
     
-    loopBeat = new Tone.Loop(song, '4n' )
+    loopBeat = new Tone.Loop(song, '16n' )
 
     // Set bpm
-    Tone.Transport.bpm.value = 140
+    // Tone.Transport.bpm.value = 140
 
     Tone.Transport.start()
     loopBeat.start(0)
@@ -22,31 +24,17 @@ function setup() {
 // ??? to fix a sample playback issue with the web and sound,
 //     do the following to the song function
 function song(time) {
-    snare.triggerAttackRelease('C4', '4n', time, 1)
-    // --- data type for current bea
-    //     BAR:BEATS:SIXTEENTH
-    let cb = [...Tone.Transport.position]
-    let bar = cb[0]
-    let beat = cb[2]
-    let sixt = cb[4]
-
-    // --- MAKE BEAT USING THE NEW VALUES ABOVE
-    // - kick
-    if (parseInt(beat) === 0) {
-        bassSynth.triggerAttackRelease('C3', '8n', time)
+    if (counter % 4 === 0) {
+        bassSynth.triggerAttackRelease("F#3", '8n', time, 1)
     }
-}
+    if (counter % 2 === 0) {
+        cymbalSynth.triggerAttackRelease('32n', time, 0.3)
+    }
 
-function stop() {
-    bassSynth.dispose()
-    polySynth.dispose()
+    counter = (counter + 1) % 16
 }
 
 // attach a click listener to a play button
 document.querySelector('#start')?.addEventListener('click', () => {
     setup()
-})
-
-document.querySelector('#end')?.addEventListener('click', () => {
-    stop()
 })
