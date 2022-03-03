137e306b17bfe81cc33165ce2bf09135d2527b94
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-02 22:57:18 -0700

    Add a stop button. Not sure if 'dispose()' is the best method to use to stop audio.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let loopBeat
+let bassSynth
 
 function setup() {
+    // Initialize the bassSynth
+    bassSynth = new Tone.MembraneSynth().toMaster()
+    
     loopBeat = new Tone.Loop(song, '4n' )
     Tone.Transport.start()
     loopBeat.start(0)
@@ -9,10 +13,19 @@ function setup() {
 // ??? to fix a sample playback issue with the web and sound,
 //     do the following to the song function
 function song(time) {
+    bassSynth.triggerAttackRelease('c3', '8n', time)
     console.log(time)
 }
 
+function stop() {
+    bassSynth.dispose()
+}
+
 // attach a click listener to a play button
-document.querySelector('button')?.addEventListener('click', () => {
+document.querySelector('#start')?.addEventListener('click', () => {
     setup()
+})
+
+document.querySelector('#end')?.addEventListener('click', () => {
+    stop()
 })
