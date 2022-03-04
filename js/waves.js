f621e2a5dbe697c846cf079b77f5a15a910b4754
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-03 22:58:53 -0700

    Adjusted how the size of the svg and the lines are handled. Seems MUCH better then before. The svg viewbox is set to 0 0 -15 100 100. The -15 helps center the sine wave

--- js/waves.js
+++ js/waves.js
@@ -1,10 +1,10 @@
 function sineWave(origin, svg) {
-    let amplitude = 10      // --- helps control height
-    let rarity = 1          // --- ??? not sure what it does yet, but higher number stretches the wave
-    let freq = 0.5          // --- controls how many waves their are
+    let amplitude = 30      // --- helps control height
+    let rarity = 2          // --- ??? not sure what it does yet, but higher number stretches the wave
+    let freq = 0.25          // --- controls how many waves their are
     let phase = 2
 
-    for (let i = -20; i < 20; i++) {
+    for (let i = -100; i < 100; i++) {
         let line = document.createElementNS("http://www.w3.org/2000/svg", "line")
 
         line.setAttribute('x1', (i - 1) * rarity + origin.x)
@@ -13,17 +13,22 @@ function sineWave(origin, svg) {
         line.setAttribute('x2', i * rarity + origin.x)
         line.setAttribute('y2', Math.sin(freq*(i + phase)) * amplitude + origin.y)
 
-        line.setAttribute('style', "stroke: black; stroke-width: 1; position: absolute; top: -10px;")
+        line.setAttribute('style', "stroke: black; stroke-width: 1;")
         svg.appendChild(line)
     }
 
     return svg
 }
 
+function sawtoothWave(origin, svg) {
+    
+}
+
 function getWaveImage(wave) {
     // create page element
     let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-    svg.setAttributeNS(null, "viewBox", "0 " + 15 + " " + 25 + " " + 25);
+    // svg.setAttributeNS(null, "viewBox", "0 " + 15 + " " + 25 + " " + 25);
+    svg.setAttributeNS(null, "viewBox", "0 -15 100 100");
     svg.setAttributeNS(null, "width", 25)
     svg.setAttributeNS(null, "height", 25)
 
