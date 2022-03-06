3f262508467f60b6f1bc51e15ad3a4415e6ff01e
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-06 01:00:55 -0700

    Cleaned up code in sequencer.js. Next step is to add a boolean to track if playing, and an array to track which sounds to play and when, when the loop is playing

--- js/sequencer.js
+++ js/sequencer.js
@@ -18,29 +18,28 @@ class Sequencer {
 
         // loop over num of rows to create cells
         for (let i = 0; i < (this.cols * this.rows); i++) {
+            // --- create a cell object and add classes
             let cell = document.createElement('div')
             cell.classList.add('cell')
-            cell.innerText = (i + 1)
 
             // --- check if cell is NOT the first one in the row
+            //     to push to the cells array with clickable cells
             if (i % this.cols !== 0) {
-                
-
+                // --- initialize a new cell object
                 let cellObj = new Cell(cell)
-
                 this.cells.push(cellObj)
-
             }
+            // --- append the current cell to the sequencer cells container
             container.appendChild(cell).className = 'sequencer-item'
         }
 
+        // add listener to clickable cells
+        // --- map over cells array and add a toggle action on click
         this.cells.map(el => {
             el.element.addEventListener('click', () => {
                 el.element.classList.toggle('active')
             })
         })
-
-        console.log(this.cells)
 
         return container
     }
@@ -49,6 +48,5 @@ class Sequencer {
 class Cell {
     constructor(element) {
         this.element = element
-
     }
 }
