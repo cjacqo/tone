ca1350889bb50c3def6204d4f3adf3ac28ec9e1b
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-06 00:56:50 -0700

    Created an array of clickable cells. This will toggle the playable array to trigger sounds

--- js/sequencer.js
+++ js/sequencer.js
@@ -2,6 +2,7 @@ class Sequencer {
     constructor() {
         this.cols = 17
         this.rows = 8
+        this.cells = []
         this.element = this.makeRows()
     }
 
@@ -18,10 +19,36 @@ class Sequencer {
         // loop over num of rows to create cells
         for (let i = 0; i < (this.cols * this.rows); i++) {
             let cell = document.createElement('div')
+            cell.classList.add('cell')
             cell.innerText = (i + 1)
+
+            // --- check if cell is NOT the first one in the row
+            if (i % this.cols !== 0) {
+                
+
+                let cellObj = new Cell(cell)
+
+                this.cells.push(cellObj)
+
+            }
             container.appendChild(cell).className = 'sequencer-item'
         }
 
+        this.cells.map(el => {
+            el.element.addEventListener('click', () => {
+                el.element.classList.toggle('active')
+            })
+        })
+
+        console.log(this.cells)
+
         return container
+    }
+}
+
+class Cell {
+    constructor(element) {
+        this.element = element
+
     }
 }
